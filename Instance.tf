56418bb20a1d6740cde515183bbb44df8714c978
chavanshubham1898 <chavanshubham1898@gmail.com>
2026-02-28 18:47:17 +0530

    added 3 instance with indexing

--- Instance.tf
+++ Instance.tf
@@ -3,9 +3,10 @@ provider "aws" {
 }
 
 resource "aws_instance" "FCT" {
-  ami = ""
+  ami = "ami-051a31ab2f4d498f5"
   instance_type = "t3.micro"
+  count = 3
   tags = {
-    Name = "FCT"
+    Name = "FCT${count.index}"
   }
 }

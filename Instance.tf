74a3c83de6cf52aeae0847e316a4d876d843db0f
chavanshubham1898 <chavanshubham1898@gmail.com>
2026-02-28 19:02:34 +0530

    adding different names with one time

--- Instance.tf
+++ Instance.tf
@@ -2,11 +2,20 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "FCT" {
+# resource "aws_instance" "FCT" {
+#   ami = "ami-051a31ab2f4d498f5"
+#   instance_type = "t3.micro"
+#   count = 3
+#   tags = {
+#     Name = "FCT${count.index+1}"
+#   }
+# }
+
+ resource "aws_instance" "FCT" {
   ami = "ami-051a31ab2f4d498f5"
+  for_each = tomap(["Jump-server","Application-server","DB-server"])
   instance_type = "t3.micro"
-  count = 3
   tags = {
-    Name = "FCT${count.index}"
+    Name = each.key
   }
 }
